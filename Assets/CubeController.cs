9b80d9046890076f39f98b79609af5acc3a3d48f
fugamiyata <72694228+fugamiyata@users.noreply.github.com>
2020-12-25 22:21:05 +0900

    Initial commit

---
+++ Assets/CubeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //キューブの移動速度
+    private float speed = -12f;
+
+    //消滅位置
+    private float deadLine = -10f;
+
+    private float vol = 0.8f;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //キューブを移動させる
+        transform.Translate(this.speed * Time.deltaTime, 0, 0);
+
+        //画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject)
+        {
+            GetComponent<AudioSource>().volume = vol;
+        }
+    }
+}
